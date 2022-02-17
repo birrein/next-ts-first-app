8f1c974fa92ecc9bea0ce065862a6f05e9651ad6
Manuel Marín <birrein@gmail.com>
2022-02-17 17:02:11 -0300

    extended expected in jes-dom

--- src/common/components/LoginForm/LoginForm.test.tsx
+++ src/common/components/LoginForm/LoginForm.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import LoginForm from '@common/components/LoginForm';
 
 describe('<LoginForm />', () => {
